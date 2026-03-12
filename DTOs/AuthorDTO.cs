ddc9fa75318a9648ca53ec43106db920e18255b4
Jolibert-git <rossaespinal01@gmail.com>
2026-03-12 01:43:48 -0400

    Cleaning up obsolete files and root folders

--- DTOs/AuthorDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Homework2.DTOs
-{
-    public class AuthorDTO
-    {
-        public int Id { set; get; }
-        public string FullName { set; get; } = string.Empty;
-    }
-}
--- DTOs/AuthorPatchDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Homework2.DTOs
-{
-    public class AuthorPatchDTO
-    {
-        public string Name { get; set; } = string.Empty;
-        public string LastNeme { get; set; } = string.Empty;
-    }
-}
